b526674738022b214c8a1d5e17b58752e8f07ab7
UkrainianUser <ostapkinpaskal@gmail.com>
2022-12-10 15:52:07 +0000

    Deploying to gh-pages from @ UkrainianUser/Project-10@cbd5bc6dac79efba5ee0f7c200ca873350eea5f1 🚀

---
+++ room.e2034d26.js
@@ -0,0 +1,2 @@
+new Swiper(".hero-swiper",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},keyboard:{enabled:!0,onlyInViewport:!0,pageUpDown:!0},mousewheel:{sensetivity:1,eventsTarget:".image-slider"},speed:800,effect:"fade",fadeEffect:{crossFade:!0}});
+//# sourceMappingURL=room.e2034d26.js.map
